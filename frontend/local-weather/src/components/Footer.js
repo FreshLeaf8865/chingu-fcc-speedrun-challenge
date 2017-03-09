115e4b4b7fa377f85f168f5ac53924ff1c9d180b
Viktors Jenovs <jviktors@gmail.com>
2017-03-09 21:42:11 +0200

    Inital commit

---
+++ frontend/local-weather/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <a href="//www.wunderground.com" target="_blank">
+          Powered by Weather Underground<br/>
+          <img width="200px" src="https://icons.wxug.com/logos/PNG/wundergroundLogo_4c_horz.png"/>
+        </a>
+      </footer>
+    )
+  }
+}
